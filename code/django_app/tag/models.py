ec40f72871f33375a00045cbb7396370240c0ac6
kaz-i-54 <torune.summer@gmail.com>
2021-09-15 04:54:59 +0000

    tag and voice model definition

--- code/django_app/tag/models.py
+++ code/django_app/tag/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Tag(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+    )
--- code/django_app/voice/models.py
+++ code/django_app/voice/models.py
@@ -1,19 +1,20 @@
 import uuid
 from django.db import models
 from tag.models import Tag
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Voice(models.Model):
     class Meta:
         db_table = "voice"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    voice = models.BinaryField(verbose_name="ウオート")
-    tag = models.ManyToManyField(Tag,verbose_name="タグ")
-    like= models.ManyToManyField(User,verbose_name="いいね")
-    like_num = models.IntegerField(defaut=0,verbose_name="いいね数")
-    created_user = models.ForeignKey(User, verbose_name="投稿者")
+    voice = models.BinaryField(verbose_name="ウオート", null=False)
+    like_num = models.IntegerField(default=0, verbose_name="いいね数")
+    created_user = models.OneToOneField(User, verbose_name="投稿者", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="投稿日時")
-    
 
+    like = models.ManyToManyField(User, verbose_name="いいね", related_name="like_people")
+    tag = models.ManyToManyField(Tag, verbose_name="タグ")
