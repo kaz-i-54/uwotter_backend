edb5e6b6f5fced68d3ccab326c5c37bfaf5b6ce6
kaz-i-54 <torune.summer@gmail.com>
2021-09-15 05:08:44 +0000

    途中経過、面談のため共有

---
+++ code/django_app/voice/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "apiv1_voice"
+
+urlpatterns = [
+    path("get_voices/", views.VoiceListAPIView.as_view()),
+    path("put_voice/", views.VoiceCreateAPIView.as_view()),
+]
--- code/django_app/voice/views.py
+++ code/django_app/voice/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import uuid
+from rest_framework import views
 
 # Create your views here.
+
+
+class VoiceListAPIView(views.APIView):
+    # VOICE001を実装する
+
+
+class VoiceCreateAPIView(views.APIView):
+    # VOICE002を実装する
