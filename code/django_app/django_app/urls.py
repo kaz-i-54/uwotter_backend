8dc784c07c8b48a7043b92ef8754a1b6e08078e8
methylpentane <v47r2bu9@gmail.com>
2021-09-15 19:39:12 +0900

    VOICE-002.
    現状はuserカラムにdjango.contrib.auth.models.Userを使用
    urls fixed.

--- code/django_app/django_app/urls.py
+++ code/django_app/django_app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/voices/", include("voice.urls")),
-    path('api/v1/tag', include('tag.urls')),
+    path("api/v1/voice/", include("voice.urls")),
+    path('api/v1/tag/', include('tag.urls')),
 
 ]
--- code/django_app/voice/views.py
+++ code/django_app/voice/views.py
@@ -1,36 +1,57 @@
-from code.django_app.voice.models import Voice
-# from django.shortcuts import render
-from rest_framework import views, status
-from rest_framework import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import VoiceSerializer
 from .models import Voice
 from tag.models import Tag
+from django.contrib.auth.models import User
 
+import base64
 # Create your views here.
 
 
-class VoiceListAPIView(views.APIView):
+class VoiceListAPIView(APIView):
     # VOICE001を実装する
     def post(requests):
         return None
 
 
-class VoiceCreateAPIView(views.APIView):
+class VoiceCreateAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        # VOCIE002
+        '''
+        input:
+        {
+            'user_uuid': text,
+            'tags': text,
+            'voice': text(base64 encoded)
+        }
+        '''
+        # no user search
+        user_uuid = request.data['user_uuid']
+        tags = request.data['tags']
+        voice = request.data['voice']
 
-        serializers = VoiceSerializer(data=request.data)
-        serializers.is_valid(raise_exception=True)
-        serializers.save()
+        voice_binary = base64.b64decode(voice)
 
-        saved_voice = serializers.instance
+        new_voice = Voice()
+        new_voice.created_user = User.objects.get(username='root')
+        new_voice.tags = Tag.objects.get(name=tags)
+        new_voice.voice = voice_binary
+        new_voice.save()
+
+        # serializers = VoiceSerializer(data=request.data)
+        # serializers.is_valid(raise_exception=True)
+        # serializers.save()
+
+        # saved_voice = serializers.instance
         # //あとでタグのリストをいれる
-        tag_id_list = []
-        for _id in tag_id_list:
-            saved_voice.tag.add(Tag.objects.gete(id=_id))
+        # tag_id_list = []
+        # for _id in tag_id_list:
+        #     saved_voice.tag.add(Tag.objects.gete(id=_id))
 
         # タグの処理をする
-        return Response(serializers.data, status.HTTP_201_CREATED)
+        # return Response(serializers.data, status.HTTP_201_CREATED)
+        return Response({'message': 'done'}, status.HTTP_201_CREATED)
 
     # テキストで送られてきたtagを分割して、新しいタグならデータベースに登録
     def save_tag(self, alltag, voice_id):
