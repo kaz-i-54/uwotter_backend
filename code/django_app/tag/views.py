f6aa83fad72b36b264d98208b7d7dc0dc4092776
kaz-i-54 <torune.summer@gmail.com>
2021-09-16 16:09:05 +0900

    TAGの一覧取得を20件に制限 & タグの多い順に並び替え

--- code/django_app/tag/views.py
+++ code/django_app/tag/views.py
@@ -5,16 +5,19 @@ from voice.models import Voice
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
+
+from django.db.models import Count
 
 
 class TagListAPIView(APIView):
     '''
     TAG001の実装
     '''
+    LIMIT_TAG_NUM = 20
 
     def get(self, request):
-        tags = Tag.objects.all()
+        tags = Tag.objects.annotate(Count("voice")) \
+            .order_by("-voice__count")[:self.LIMIT_TAG_NUM]
         serializer = TagListSerializer(tags, many=True)
         response_json = {
             "result": list(serializer.data)
