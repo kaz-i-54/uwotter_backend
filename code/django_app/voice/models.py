b0daae1dabf464ec8c0ddbba437f418900c7d49f
kaz-i-54 <torune.summer@gmail.com>
2021-09-16 20:02:35 +0900

    adminページでObject名が見える&Voiceモデルデフォルト値変更(migration need)

--- code/django_app/voice/models.py
+++ code/django_app/voice/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from tag.models import Tag
 # from django.contrib.auth.models import User
 from user.models import MyUser
+from django.utils import timezone
 
 # Create your models here.
 
@@ -15,7 +16,7 @@ class Voice(models.Model):
     voice = models.BinaryField(verbose_name="ウオート", null=False)
     like_num = models.IntegerField(default=0, verbose_name="いいね数")
     created_user = models.ForeignKey(MyUser, verbose_name="投稿者", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="投稿日時")
+    created_at = models.DateTimeField(default=timezone.now(), verbose_name="投稿日時")
 
     like = models.ManyToManyField(MyUser, verbose_name="いいね", related_name="like_people")
     tag = models.ManyToManyField(Tag, verbose_name="タグ")
