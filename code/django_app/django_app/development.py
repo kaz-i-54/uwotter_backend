899a5482558dc2d96750f79a82c19e3a91840f61
kaz-i-54 <torune.summer@gmail.com>
2021-09-17 03:33:47 +0900

    static fileの設定

--- code/django_app/django_app/development.py
+++ code/django_app/django_app/development.py
@@ -139,6 +139,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+## heroku用に追記
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
