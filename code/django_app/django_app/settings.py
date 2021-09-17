29d0da009c271e1b94e38a971c93376e05697f77
methylpentane <v47r2bu9@gmail.com>
2021-09-17 12:40:35 +0900

    Revert "herokuの準備"
    This reverts commit 62ecbf9db7f5318ea5d0275a7b1288aa85bba6aa.

--- code/django_app/django_app/settings.py
+++ code/django_app/django_app/settings.py
@@ -84,22 +84,13 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': 'secret',
-        'HOST': 'db',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
